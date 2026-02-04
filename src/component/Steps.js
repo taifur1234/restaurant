9f04562b2cbe4f53c2b6298e8b25b62e0e66f084
taifurahmad <taifurahmad24@gmail.com>
2026-02-04 20:56:33 +0530

    commit

--- src/component/Steps.js
+++ src/component/Steps.js
@@ -11,7 +11,7 @@ export default function Steps() {
       <p className='subtitle'>----- Reserve your table in just 3 easy steps -----</p>
 
       <div className='steps-box'>
-        {/* Step 1 */}
+        
         <div className='step-card'>
           <div className='step-icon'>1</div>
           <img className='step-one-img' src='step-1-img.png' alt=''/>
@@ -19,7 +19,7 @@ export default function Steps() {
           <p>Select the date and time that <br/> suits you best for your visit.</p>
         </div>
 
-        {/* Step 2 */}
+      
         <div className='step-card'>
           <div className='step-icon'>2</div>
           <img className='step-one-img' src='step-2-img.jpg' alt=''/>
@@ -27,7 +27,7 @@ export default function Steps() {
           <p>Pick a table from our available <br/> options to reserve your spot.</p>
         </div>
 
-        {/* Step 3 */}
+      
         <div className='step-card'>
           <div className='step-icon'>3</div>
           <img className='step-one-img' src='step-3-imgg.jfif' alt=''/>
